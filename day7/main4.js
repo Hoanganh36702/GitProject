28c0aae9ed344c4632639565566dd6667041749d
titv <Hoanganh36702@gmail.com>
2023-11-14 13:14:58 +0700

    add bai tap tinh dien tich hinh chu nhap

---
+++ day7/main4.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputLength;
+
+inputWidth = prompt("Vui lòng nhập chiều rộng: ")
+inputLength = prompt("Vui lòng nhập chiều dài: ")
+
+let area = inputWidth * inputLength;
+document.write("The area is: " + area);
